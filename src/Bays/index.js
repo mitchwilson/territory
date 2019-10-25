2e6e0f3c31565b378b999e0bc2940f3e19da42fb
Mitch Wilson <mitchwilson1969@gmail.com>
2019-10-25 09:53:17 -0500

    Add three white pawns

--- src/Bays/index.js
+++ src/Bays/index.js
@@ -5,6 +5,7 @@ import Piece from '../Board/Piece'
 import BlackKingImage from '../images/Chess_kdt60.png'
 import WhiteKingImage from '../images/Chess_klt60.png'
 import BlackPawnImage from '../images/Chess_pdt60.png'
+import WhitePawnImage from '../images/Chess_plt60.png'
 
 const wrapperStyle = {
   width: "100%",
@@ -28,12 +29,15 @@ export default class Bays extends React.Component {
       <div style={wrapperStyle}>
         <Droppable id="dr1" style={droppableStyle} drop={this.drop}>
           <Draggable id="item1"><Piece image={WhiteKingImage} altText='White King' /></Draggable>
+          <Draggable id="item2"><Piece image={WhitePawnImage} altText='White Pawn 1' /></Draggable>
+          <Draggable id="item3"><Piece image={WhitePawnImage} altText='White Pawn 2' /></Draggable>
+          <Draggable id="item4"><Piece image={WhitePawnImage} altText='White Pawn 3' /></Draggable>
         </Droppable>
         <Droppable id="dr2" style={droppableStyle} drop={this.drop}>
-          <Draggable id="item2"><Piece image={BlackKingImage} altText='Black King' /></Draggable>
-          <Draggable id="item3"><Piece image={BlackPawnImage} altText='Black Pawn 1' /></Draggable>
-          <Draggable id="item4"><Piece image={BlackPawnImage} altText='Black Pawn 2' /></Draggable>
-          <Draggable id="item5"><Piece image={BlackPawnImage} altText='Black Pawn 3' /></Draggable>
+          <Draggable id="item5"><Piece image={BlackKingImage} altText='Black King' /></Draggable>
+          <Draggable id="item6"><Piece image={BlackPawnImage} altText='Black Pawn 1' /></Draggable>
+          <Draggable id="item7"><Piece image={BlackPawnImage} altText='Black Pawn 2' /></Draggable>
+          <Draggable id="item8"><Piece image={BlackPawnImage} altText='Black Pawn 3' /></Draggable>
         </Droppable>
       </div>
     )
