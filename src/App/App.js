273d595b623e800a12c2d48e1a6ea7b1ce0b8aa2
Mitch Wilson <mitchwilson1969@gmail.com>
2020-04-25 16:04:26 -0500

    Move Header files into App folder because app uses them

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Board from '../Board/Board'
-import Header from '../Header/Header'
+import Header from './Header'
 import './App.css'
 
 class App extends React.Component {
